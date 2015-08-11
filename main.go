f65af118ca769ae84cb7f773263eb77926a34c7b
Robert Marwil <rjm02006@gmail.com>
2015-08-11 15:40:02 -0400

    remove whitespace

--- main.go
+++ main.go
@@ -16,9 +16,8 @@ func beyondHello() {
   y := 4
   sum, prod := learnMultiple(x, y)
   fmt.Println("sum:", sum, "prod:", prod)
-
 }
 
-func learnMultiple(x, y int) (int, int) {
+func learnMultiple(x, y int) (sum, prod int) {
   return x + y, x * y
 }
